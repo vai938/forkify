8c77314d11a673108153dfb0ca4467f211e71958
vai938 <vaibhavkp95207@gmail.com>
2022-05-24 20:18:53 +0530

    Initial commit

---
+++ src/js/resultView.js
@@ -0,0 +1,29 @@
+import View from './veeiw.js';
+import icons from 'url:../img/icons.svg'; // Parcel 2.0
+
+class ResultView extends View {
+  _parentElement = document.querySelector('.results');
+  _generateMarkup() {
+    return this._data.map(this._generateMarkupPreview).join('')
+  }
+
+  _generateMarkupPreview(result) {
+    const id = window.location.hash.slice(1);
+
+    return `
+        <li class="preview">
+        <a class="preview__link ${result.id === id ? 'preview__link--active' : ''} " href="#${result.id}">
+          <figure class="preview__fig">
+            <img src="${result.image}" alt="Test" />
+          </figure>
+          <div class="preview__data">
+            <h4 class="preview__title">${result.title}</h4>
+            <p class="preview__publisher">${result.publisher}</p>
+          </div>
+        </a>
+      </li>
+        `;
+  }
+}
+
+export default new ResultView();
